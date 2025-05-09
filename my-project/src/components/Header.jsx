bd367cbc2641731d6fb296b4dce1ae393682aab6
Aidan <aidanlmcmurray@gmail.com>
2025-05-09 13:51:57 -0700

    home tag

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -1,24 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <>
-      <div className="flex justify-center items-center gap-2 text-center">
-        <h1 className="font-roobert font-bold text-[14px]">SKINSTRIC</h1>
-        <img
-          src="assets/Rectangle-2710.svg"
-          alt=""
-          className="text-skinstric-gray-light h-[17px]"
-        />
-        <h2 className="font-roobert text-skinstric-gray-light text-[10px] text-bold">
-          INTRO
-        </h2>
-        <img
-          src="assets/Rectangle-2711.svg"
-          alt=""
-          className="text-skinstric-gray-light h-[17px]"
-        />
-      </div>
+      <Link to='/'>
+        <div className="flex justify-center items-center gap-2 text-center">
+          <h1 className="font-roobert font-bold text-[14px]">SKINSTRIC</h1>
+          <img
+            src="assets/Rectangle-2710.svg"
+            alt=""
+            className="text-skinstric-gray-light h-[17px]"
+          />
+          <h2 className="font-roobert text-skinstric-gray-light text-[10px] text-bold">
+            INTRO
+          </h2>
+          <img
+            src="assets/Rectangle-2711.svg"
+            alt=""
+            className="text-skinstric-gray-light h-[17px]"
+          />
+        </div>
+      </Link>
     </>
   );
 }
